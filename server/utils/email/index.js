a211f67c8e03eacb4ce49e79bb5c6a023bb024c9
HARSHisawesome2 <103326606+HARSHisawesome2@users.noreply.github.com>
2022-08-29 22:42:02 +0530

    Update index.js

--- server/utils/email/index.js
+++ server/utils/email/index.js
@@ -1,28 +1,36 @@
-const nodemailer = require('nodemailer');
-
-const { EMAIL_SENDER, MAIL_HOST, MAIL_USER, MAIL_PASS } = process.env;
-
-const sendEmail = async (to, subject, html) => {
+const nodemailer = require("nodemailer");
+const sendEmail = async (email, subject, text) => {
   try {
-    const transport = nodemailer.createTransport({
-      host: MAIL_HOST,
-      port: 2525,
-      // secure: true,
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email' , 
+      port: 587,
       auth: {
-        user: MAIL_USER,
-        pass: MAIL_PASS,
+          user: 'merritt.nicolas80@ethereal.email',
+          pass: 'hCWX4H3FRaf16pFCcZ'
       },
     });
 
-    await transport.sendMail({
-      from: EMAIL_SENDER,
-      to,
-      subject,
-      html,
+    await transporter.sendMail({
+      from: 'merritt.nicolas80@ethereal.email',
+      to: email,
+      subject: subject,
+      text: text,
     });
+    console.log("email sent sucessfully");
   } catch (error) {
+    console.log("email not sent");
     console.log(error);
   }
 };
 
 module.exports = sendEmail;
+
+
+
+
+
+
+
+
+
+
